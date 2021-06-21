14740ca42ff777267945a0634a974cf263e73558
nepozs <56661567+nepozs@users.noreply.github.com>
2021-06-21 22:30:10 +0200

    Add manufacturers (#31)
    * Update const.py
    
    Ikea and Osram brand mapping
    
    * Update const.py

--- custom_components/powercalc/const.py
+++ custom_components/powercalc/const.py
@@ -19,5 +19,7 @@ MODE_LINEAR = "linear"
 MODE_FIXED = "fixed"
 
 MANUFACTURER_DIRECTORY_MAPPING = {
+    "IKEA of Sweden": "ikea",
+    "OSRAM": "osram",
     "Signify Netherlands B.V.": "signify"
 }
