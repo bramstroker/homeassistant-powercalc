a91140e2fb4cd85c2399a2618abfb93bf32a2035
Bram Gerritsen <bgerritsen@emico.nl>
2021-06-21 16:57:24 +0200

    Use absolute paths

--- .github/scripts/lut_validator/validate.js
+++ .github/scripts/lut_validator/validate.js
@@ -3,9 +3,10 @@ const readdirp = require('readdirp');
 const { ungzip } = require('node-gzip');
 const fs = require('fs');
 const { exit } = require('process');
+const path = require('path');
 
 const main = async () => {
-  const dataDirectory = '../../../custom_components/powercalc/data'
+  const dataDirectory = path.join(__dirname, '../../../custom_components/powercalc/data')
   let hasError = false
   for await (const file of readdirp(dataDirectory, {fileFilter: '*.csv.gz'})) {
     const colorMode = file.basename.substring(0, file.basename.indexOf('.csv.gz'))
@@ -14,7 +15,7 @@ const main = async () => {
     const gzipped = fs.readFileSync(file.fullPath)
     const csvBuffer = await ungzip(gzipped)
     const parsed = await csval.parseCsv(csvBuffer.toString());
-    const rules = await csval.readRules('rules/' + colorMode + '.json');
+    const rules = await csval.readRules(path.join(__dirname, 'rules/' + colorMode + '.json'));
     try {
       await csval.validate(parsed, rules)
     } catch (ex) {
