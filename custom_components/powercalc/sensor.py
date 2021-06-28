329f56ea7c2128650c39ada0ce231e83a09c0f66
Bram Gerritsen <bgerritsen@emico.nl>
2021-06-28 16:38:58 +0200

    Add support for input_boolean entities

--- custom_components/powercalc/sensor.py
+++ custom_components/powercalc/sensor.py
@@ -16,6 +16,7 @@ from homeassistant.components import (
     media_player,
     remote,
     switch,
+    input_boolean
 )
 from homeassistant.components.hue.const import DOMAIN as HUE_DOMAIN
 from homeassistant.components.light import PLATFORM_SCHEMA, Light
@@ -88,6 +89,7 @@ PLATFORM_SCHEMA = vol.All(
                     binary_sensor.DOMAIN,
                     remote.DOMAIN,
                     media_player.DOMAIN,
+                    input_boolean.DOMAIN
                 )
             ),
             vol.Optional(CONF_MODEL): cv.string,
