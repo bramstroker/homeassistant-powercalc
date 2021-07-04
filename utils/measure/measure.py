b2cfbc3d6f1195105adb07cdf7dba7f63c159157
Bram Gerritsen <bgerritsen@emico.nl>
2021-07-04 14:36:49 +0200

    Fix type error on python 3.8

--- utils/measure/measure.py
+++ utils/measure/measure.py
@@ -1,4 +1,4 @@
-from __future__ import print_function, unicode_literals
+from __future__ import print_function, unicode_literals, annotations
 from PyInquirer import prompt
 from typing import Iterator
 from aiohue.lights import Light
