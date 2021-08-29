d65170aed6dd38294ac7548a21f23d2a1b017708
Bram Gerritsen <bgerritsen@emico.nl>
2021-08-29 17:45:41 +0200

    Implement kasa

--- utils/measure_v2/powermeter/kasa.py
+++ utils/measure_v2/powermeter/kasa.py
@@ -1,5 +1,16 @@
+from kasa import SmartPlug
+import asyncio
 from .powermeter import PowerMeter
 
 class KasaPowerMeter(PowerMeter):
+    def __init__(self, device_ip: str):
+        self._smartplug = SmartPlug(device_ip)
+
     def get_power(self) -> float:
-        return 0
+        loop = asyncio.get_event_loop()
+        power = loop.run_until_complete(self.async_read_power_meter())
+        return power
+    
+    async def async_read_power_meter(self):
+        await self._smartplug.update()
+        return self._smartplug.emeter_realtime['power']
