e9fe01d78ac57cbc36c3dfca63863cdb2e33f0f6
Bram Gerritsen <bgerritsen@emico.nl>
2021-09-18 07:45:21 +0200

    Commit WIP

--- utils/measure_v2/playground/test_zwave copy.py
+++ utils/measure_v2/playground/test_zwave copy.py
@@ -12,18 +12,19 @@ import queue
 from zwave_js_server.client import Client
 from zwave_js_server.model.driver import Driver
 
-ZWAVE_JS_URL = "ws://192.168.1.188:3000"
-NODE_ID = 14
+ZWAVE_JS_URL = "ws://192.168.178.99:3000"
+NODE_ID = 28
 
 class ZwaveJsPowerMeter():
     def __init__(self, ws_url: str):
         self._power: float = None
         self._ws_url: str = ws_url
         self._node_id: int = NODE_ID
+        self._connected: bool = False
 
         thread = threading.Thread(target=self.start_monitor)
         thread.start()
-        thread.join()
+        #thread.join()
 
     def start_monitor(self):
         asyncio.run(self.connect())
@@ -33,9 +34,57 @@ class ZwaveJsPowerMeter():
             async with Client(self._ws_url, session) as client:
                 driver_ready = asyncio.Event()
                 asyncio.create_task(self.on_driver_ready(client, driver_ready))
-
                 await client.listen(driver_ready)
 
+    async def connect2(self):
+        """Connect to the server."""
+        self.session = aiohttp.ClientSession()
+        self._client = Client(self._ws_url, self.session)
+
+        #self._client = client
+        #driver_ready = asyncio.Event()
+        #asyncio.create_task(self.on_driver_ready(client, driver_ready))
+
+        await self._client.connect()
+        await self._client.set_api_schema()
+        await self._client._send_json_message(
+            {
+                "command": "driver.get_log_config",
+                "messageId": "get-initial-log-config",
+            }
+        )
+        log_msg = await self._client._receive_json_or_raise()
+
+        # this should not happen, but just in case
+        if not log_msg["success"]:
+            await self._client.close()
+            #raise FailedCommand(log_msg["messageId"], log_msg["errorCode"])
+
+        # send start_listening command to the server
+        # we will receive a full state dump and from now on get events
+        await self._client._send_json_message(
+            {"command": "start_listening", "messageId": "listen-id"}
+        )
+
+        state_msg = await self._client._receive_json_or_raise()
+
+        if not state_msg["success"]:
+            await self._client.close()
+            #raise FailedCommand(state_msg["messageId"], state_msg["errorCode"])
+
+        loop = asyncio.get_running_loop()
+        driver = cast(
+            Driver,
+            await loop.run_in_executor(
+                None,
+                Driver,
+                self._client,
+                state_msg["result"]["state"],
+                log_msg["result"]["config"],
+            ),
+        )
+        self.driver = driver
+        self._connected = True
 
     async def on_driver_ready(self, client: Client, driver_ready: asyncio.Event) -> None:
         """Act on driver ready."""
@@ -50,7 +99,7 @@ class ZwaveJsPowerMeter():
                 
     async def get_power(self) -> float:
         if self._connected == False:
-            await self.connect()
+            await self.connect2()
 
         client = self._client
         client.driver = self.driver
@@ -59,8 +108,9 @@ class ZwaveJsPowerMeter():
 
         power_values = [v for v in node.values.values() if v.metadata.unit == "W"]
         self.node_value = power_values[0]
+        print("Sending refresh values")
         await node.async_refresh_cc_values(CommandClass(self.node_value.command_class))
-
+        await asyncio.sleep(1)
         node = client.driver.controller.nodes.get(self._node_id)
 
         power_values = [v for v in node.values.values() if v.metadata.unit == "W"]
@@ -90,9 +140,15 @@ class ZwaveJsPowerMeter():
         if value != self.node_value:
             return
 
-        print("retrieved power")
+        print("New value")
         power = value.value
         print(power)
         self._power = power
 
-ZwaveJsPowerMeter(ZWAVE_JS_URL)
+async def _main():
+    meter = ZwaveJsPowerMeter(ZWAVE_JS_URL)
+    while True:
+        await asyncio.sleep(2)
+        await meter.get_power()
+
+asyncio.run(_main())
