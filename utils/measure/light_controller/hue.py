3c9359bdfd599f49535ce8bcd96f1e5463109f11
Bram Gerritsen <bgerritsen@gmail.com>
2021-10-02 16:33:44 +0200

    Measure tool: Control hue groups (#280)

--- utils/measure/light_controller/hue.py
+++ utils/measure/light_controller/hue.py
@@ -3,11 +3,14 @@ from __future__ import annotations
 import os
 
 from phue import Bridge, PhueRegistrationException
+from PyInquirer import Separator
 
 from .controller import LightController, LightInfo
+from .errors import LightControllerError, ModelNotDiscoveredError
 
 NAME = "hue"
-
+TYPE_LIGHT = "light"
+TYPE_GROUP = "group"
 
 class HueLightController(LightController):
     def __init__(self, bridge_ip: str):
@@ -15,9 +18,19 @@ class HueLightController(LightController):
 
     def change_light_state(self, color_mode: str, on: bool = True, **kwargs):
         kwargs["on"] = on
-        self.bridge.set_light(self.light_id, kwargs)
+        if self.is_group:
+            self.bridge.set_group(self.light_id, kwargs)
+        else:
+            self.bridge.set_light(self.light_id, kwargs)
 
     def get_light_info(self) -> LightInfo:
+        if self.is_group:
+            model_id = self.find_group_model(self.light_id)
+            return LightInfo(
+                model_id=model_id,
+            )
+        
+        # Individual light information
         light = self.bridge.get_light(self.light_id)
         lightinfo = LightInfo(
             model_id=light["modelid"],
@@ -28,6 +41,22 @@ class HueLightController(LightController):
             lightinfo.max_mired = light["capabilities"]["control"]["ct"]["max"]
 
         return lightinfo
+
+    def find_group_model(self, group_id: str) -> str:
+        model_ids = set()
+        for light_id in self.bridge.get_group(group_id, "lights"):
+            light = self.bridge.get_light(int(light_id))
+            model_id = light["modelid"]
+            model_ids.add(model_id)
+
+        if len(model_ids) == 0:
+            raise ModelNotDiscoveredError("Could not find a model id for the group")
+        
+        if len(model_ids) > 1:
+            raise LightControllerError("The Hue group contains lights of multiple models, this is not supported")
+        
+        return model_ids.pop()
+
 
     def initialize_hue_bridge(self, bridge_ip: str) -> Bridge:
         config_file_path = os.path.join(os.path.dirname(__file__), "../.persistent/.python_hue")
@@ -41,20 +70,32 @@ class HueLightController(LightController):
         return bridge
 
     def get_questions(self) -> list[dict]:
-        light_list = []
-        for light in self.bridge.lights:
-            light_list.append(
-                {"key": light.light_id, "value": light.light_id, "name": light.name}
-            )
+
+        def get_light_list(answers):
+            light_list = []
+            for light in self.bridge.lights:
+                light_list.append(
+                    {"value": f"{TYPE_LIGHT}:{light.light_id}", "name": light.name}
+                )
+            if answers["multiple_lights"]:
+                light_list.append(Separator())
+                for group in self.bridge.groups:
+                    light_list.append(
+                        {"value": f"{TYPE_GROUP}:{group.group_id}", "name": group.name}
+                    )
+
+            return light_list
 
         return [
             {
                 "type": "list",
                 "name": "light",
                 "message": "Select the light?",
-                "choices": light_list,
+                "choices": get_light_list,
             },
         ]
 
     def process_answers(self, answers):
-        self.light_id = answers["light"]
+        light_type, light_id = answers["light"].split(":")
+        self.is_group = light_type == TYPE_GROUP
+        self.light_id = int(light_id)
