763a4488fb3322e5ee0b1e88db6833c50aa5f586
Bram Gerritsen <bgerritsen@emico.nl>
2021-11-05 17:37:44 +0100

    Compatbility with new utility meter implementation

--- custom_components/powercalc/sensors/utility_meter.py
+++ custom_components/powercalc/sensors/utility_meter.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 
+import inspect
 import logging
 from typing import Union
 
-from awesomeversion import AwesomeVersion
-from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.components.utility_meter import DEFAULT_OFFSET
-from homeassistant.components.utility_meter import DOMAIN as UTILITY_DOMAIN
 from homeassistant.components.utility_meter.const import (
     CONF_METER_NET_CONSUMPTION,
     CONF_METER_TYPE,
@@ -17,7 +15,6 @@ from homeassistant.components.utility_meter.const import (
 )
 from homeassistant.components.utility_meter.sensor import UtilityMeterSensor
 from homeassistant.const import __short_version__
-from homeassistant.helpers import discovery
 from homeassistant.helpers.typing import HomeAssistantType
 
 from custom_components.powercalc.const import (
@@ -47,39 +44,33 @@ def create_utility_meters(
         entity_id = f"{energy_sensor.entity_id}_{meter_type}"
         _LOGGER.debug("Creating utility_meter sensor: %s", name)
 
-        # Below is for BC purposes. Can be removed somewhere in the future
-        if AwesomeVersion(__short_version__) < "2021.10":
-            utility_meter = VirtualUtilityMeterSensor(
-                energy_sensor.entity_id, name, meter_type, entity_id
-            )
-        else:
-            if not DATA_UTILITY in hass.data:
-                hass.data[DATA_UTILITY] = {}
-            hass.data[DATA_UTILITY][entity_id] = {
-                CONF_SOURCE_SENSOR: energy_sensor.entity_id,
-                CONF_METER_TYPE: meter_type,
-                CONF_TARIFFS: [],
-                CONF_METER_NET_CONSUMPTION: False,
-            }
+        if not DATA_UTILITY in hass.data:
+            hass.data[DATA_UTILITY] = {}
+        hass.data[DATA_UTILITY][entity_id] = {
+            CONF_SOURCE_SENSOR: energy_sensor.entity_id,
+            CONF_METER_TYPE: meter_type,
+            CONF_TARIFFS: [],
+            CONF_METER_NET_CONSUMPTION: False,
+        }
 
-            utility_meter = UtilityMeterSensor(
-                parent_meter=entity_id,
-                source_entity=energy_sensor.entity_id,
-                name=name,
-                meter_type=meter_type,
-                meter_offset=DEFAULT_OFFSET,
-                net_consumption=False,
-            )
+        params = {
+            "source_entity": energy_sensor.entity_id,
+            "name": name,
+            "meter_type": meter_type,
+            "meter_offset": DEFAULT_OFFSET,
+            "net_consumption": False,
+        }
 
-            hass.data[DATA_UTILITY][entity_id][DATA_TARIFF_SENSORS] = [utility_meter]
-        utility_meters.append(utility_meter)
+        signature = inspect.signature(UtilityMeterSensor.__init__)
+        if "parent_meter" in signature.parameters:
+            params["parent_meter"] = entity_id
+        if "delta_values" in signature.parameters:
+            params["delta_values"] = False
+
+
+        utility_meter = UtilityMeterSensor(**params)
+
+        hass.data[DATA_UTILITY][entity_id][DATA_TARIFF_SENSORS] = [utility_meter]
+    utility_meters.append(utility_meter)
 
     return utility_meters
-
-
-class VirtualUtilityMeterSensor(UtilityMeterSensor):
-    """Utility meter resets on each cycle (daily, hourly etc)"""
-
-    def __init__(self, source_entity, name, meter_type, entity_id):
-        super().__init__(source_entity, name, meter_type, DEFAULT_OFFSET, False)
-        self.entity_id = entity_id
