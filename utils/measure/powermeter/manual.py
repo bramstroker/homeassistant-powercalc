fce4a994c96246bef36305ef27184a83d898e30d
Bram Gerritsen <bgerritsen@emico.nl>
2021-11-07 10:58:04 +0100

    Manual measurements can be inputed in ampere now

--- utils/measure/powermeter/manual.py
+++ utils/measure/powermeter/manual.py
@@ -4,9 +4,47 @@ import time
 
 from .powermeter import PowerMeasurementResult, PowerMeter
 
+UNIT_WATT = "W"
+UNIT_AMPERE = "A"
+UNIT_MILI_AMPERE = "mA"
+UNIT_VOLTAGE = "V"
 
 class ManualPowerMeter(PowerMeter):
+    def __init__(self):
+        self._unit_of_measurement: str = UNIT_WATT
+        self._voltage: int = None
+
     def get_power(self) -> PowerMeasurementResult:
         print('Input power measurement:')
-        power = input()
-        return PowerMeasurementResult(float(power), time.time())
+        power = float(input())
+        if self._unit_of_measurement == UNIT_AMPERE:
+            power = power * self._voltage
+        if self._unit_of_measurement == UNIT_MILI_AMPERE:
+            power = power / 1000 * self._voltage
+
+        return PowerMeasurementResult(power, time.time())
+    
+    def get_questions(self) -> list[dict]:
+        return [
+            {
+                "type": "list",
+                "name": "unit_of_measurement",
+                "message": "What is your unit of measurements?",
+                "default": UNIT_WATT,
+                "choices": [
+                    UNIT_WATT,
+                    UNIT_AMPERE,
+                    UNIT_MILI_AMPERE
+                ]
+            },
+            {
+                "type": "input",
+                "name": "voltage",
+                "message": "Input the voltage",
+                "when": lambda answers: answers["unit_of_measurement"] != UNIT_WATT,
+            },
+        ]
+
+    def process_answers(self, answers):
+        self._unit_of_measurement = answers["unit_of_measurement"]
+        self._voltage = int(answers["voltage"])
