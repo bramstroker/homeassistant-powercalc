7287396d6e84117b418d7dafbe157aea2766f8b7
Bram Gerritsen <bgerritsen@emico.nl>
2022-01-16 10:22:51 +0100

    Add possibility to use daily fixed energy sensor in a group

--- custom_components/powercalc/sensor.py
+++ custom_components/powercalc/sensor.py
@@ -2,8 +2,8 @@
 
 from __future__ import annotations
 
-import copy
 import logging
+import uuid
 from datetime import timedelta
 from typing import Final, cast
 
@@ -98,7 +98,7 @@ from .errors import (
     SensorConfigurationError,
 )
 from .model_discovery import is_supported_model
-from .sensors.energy import DailyEnergySensor, VirtualEnergySensor, create_energy_sensor
+from .sensors.energy import DailyEnergySensor, EnergySensor, create_energy_sensor
 from .sensors.group import GroupedEnergySensor, GroupedPowerSensor, GroupedSensor
 from .sensors.power import PowerSensor, RealPowerSensor, create_power_sensor
 from .sensors.utility_meter import create_utility_meters
@@ -241,6 +241,9 @@ def get_merged_sensor_configuration(*configs: dict) -> dict:
         merged_config[CONF_CREATE_ENERGY_SENSOR] = merged_config.get(
             CONF_CREATE_ENERGY_SENSORS
         )
+    
+    if CONF_DAILY_FIXED_ENERGY in merged_config:
+        merged_config[CONF_ENTITY_ID] = DUMMY_ENTITY_ID
 
     if not CONF_ENTITY_ID in merged_config:
         raise SensorConfigurationError(
@@ -278,9 +281,6 @@ async def create_sensors(
     existing_sensors = []
     if CONF_ENTITIES in config:
         for entity_config in config[CONF_ENTITIES]:
-            if CONF_ENTITY_ID in entity_config:
-                sensor_configs.update({entity_config[CONF_ENTITY_ID]: entity_config})
-
             # When there are nested entities, combine these with the current entities, resursively
             if CONF_ENTITIES in entity_config:
                 (child_new_sensors, child_existing_sensors) = await create_sensors(
@@ -288,6 +288,10 @@ async def create_sensors(
                 )
                 new_sensors.extend(child_new_sensors)
                 existing_sensors.extend(child_existing_sensors)
+                continue
+            
+            entity_id = entity_config.get(CONF_ENTITY_ID) or str(uuid.uuid4())
+            sensor_configs.update({entity_id: entity_config})
 
     # Automatically add a bunch of entities by area or evaluating template
     if CONF_INCLUDE in config:
@@ -432,7 +436,7 @@ async def create_group_sensors(
     _LOGGER.debug("Creating grouped power sensor: %s", name)
 
     energy_sensors = list(
-        filter(lambda elm: isinstance(elm, VirtualEnergySensor), entities)
+        filter(lambda elm: isinstance(elm, EnergySensor), entities)
     )
     energy_sensor_ids = list(map(lambda x: x.entity_id, energy_sensors))
     name_pattern = sensor_config.get(CONF_ENERGY_SENSOR_NAMING)
