f655402098dff7de66ab16b35c02e8fdf1fc735f
Bram Gerritsen <bgerritsen@emico.nl>
2022-02-11 09:35:39 +0100

    Fix utility meter entity_id is derived from energy meter entity_id not name

--- custom_components/powercalc/sensors/utility_meter.py
+++ custom_components/powercalc/sensors/utility_meter.py
@@ -4,6 +4,7 @@ import inspect
 import logging
 from typing import Union
 
+from homeassistant.components.sensor import ENTITY_ID_FORMAT
 from homeassistant.components.utility_meter.const import (
     CONF_METER_NET_CONSUMPTION,
     CONF_METER_TYPE,
@@ -14,14 +15,19 @@ from homeassistant.components.utility_meter.const import (
 )
 from homeassistant.components.utility_meter.sensor import UtilityMeterSensor
 from homeassistant.const import __short_version__
+from homeassistant.helpers.entity import async_generate_entity_id
 from homeassistant.helpers.typing import HomeAssistantType
+from homeassistant.helpers.entity_registry import (
+    EntityRegistry,
+    async_get,
+)
 
 from custom_components.powercalc.const import (
     CONF_CREATE_UTILITY_METERS,
     CONF_UTILITY_METER_OFFSET,
     CONF_UTILITY_METER_TYPES,
 )
-from custom_components.powercalc.migrate import async_set_unique_id
+from custom_components.powercalc.migrate import async_set_unique_id, async_migrate_entity_id
 from custom_components.powercalc.sensors.energy import (
     DailyEnergySensor,
     VirtualEnergySensor,
@@ -46,7 +52,7 @@ async def create_utility_meters(
     for meter_type in meter_types:
         name = f"{energy_sensor.name} {meter_type}"
         entity_id = f"{energy_sensor.entity_id}_{meter_type}"
-        _LOGGER.debug("Creating utility_meter sensor: %s", name)
+        _LOGGER.debug(f"Creating utility_meter sensor: {name} (entity_id={entity_id})")
 
         if not DATA_UTILITY in hass.data:
             hass.data[DATA_UTILITY] = {}
@@ -78,6 +84,15 @@ async def create_utility_meters(
             # Set new unique id if this entity already exists in the entity registry
             async_set_unique_id(hass, entity_id, unique_id)
             utility_meter.unique_id = unique_id
+        
+        # Migrate entity_id to new naming
+        old_entity_id = async_generate_entity_id(ENTITY_ID_FORMAT, name, hass=hass)
+        ent_reg = async_get(hass)
+        if ent_reg.async_get(old_entity_id) and entity_id != old_entity_id:
+            _LOGGER.debug(f"Migrating utility_meter entity_id from {old_entity_id} to {entity_id}")
+            ent_reg.async_update_entity(old_entity_id, new_entity_id=entity_id)
+
+        utility_meter.entity_id = entity_id
 
         hass.data[DATA_UTILITY][entity_id][DATA_TARIFF_SENSORS] = [utility_meter]
         utility_meters.append(utility_meter)
