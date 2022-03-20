3699825bfc717d687f70991eaf615932d434471d
Bram Gerritsen <bgerritsen@emico.nl>
2022-03-20 20:50:41 +0100

    Fix unexpected keyword argument 'device_id'

--- custom_components/powercalc/sensors/energy.py
+++ custom_components/powercalc/sensors/energy.py
@@ -165,7 +165,18 @@ class VirtualEnergySensor(IntegrationSensor, EnergySensor):
         powercalc_source_entity: str,
         powercalc_source_domain: str,
     ):
-        if AwesomeVersion(HA_VERSION) < AwesomeVersion("2022.4"):
+        if AwesomeVersion(HA_VERSION) >= AwesomeVersion("2022.4.0.dev0"):
+            super().__init__(
+                source_entity=source_entity,
+                name=name,
+                round_digits=round_digits,
+                unit_prefix=unit_prefix,
+                unit_time=unit_time,
+                unit_of_measurement=unit_of_measurement,
+                integration_method=integration_method,
+                unique_id=unique_id,
+            )
+        else:
             super().__init__(
                 source_entity=source_entity,
                 name=name,
@@ -177,17 +188,6 @@ class VirtualEnergySensor(IntegrationSensor, EnergySensor):
             )
             if unique_id:
                 self._attr_unique_id = unique_id
-        else:
-            super().__init__(
-                source_entity=source_entity,
-                name=name,
-                round_digits=round_digits,
-                unit_prefix=unit_prefix,
-                unit_time=unit_time,
-                unit_of_measurement=unit_of_measurement,
-                integration_method=integration_method,
-                unique_id=unique_id,
-            )
 
         self._powercalc_source_entity = powercalc_source_entity
         self._powercalc_source_domain = powercalc_source_domain
