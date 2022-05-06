dd53bdbc59364c3998ad3b4686dcd93a1852010b
Bram <bgerritsen@emico.nl>
2022-05-06 10:08:18 +0200

    Remove clutter

--- utils/measure/ocr/main.py
+++ utils/measure/ocr/main.py
@@ -6,24 +6,10 @@ import ocr
 def main():
     """
     Handles command line arguments and begins the real-time OCR by calling ocr_stream().
-    A path to the Tesseract cmd root is required, but all other params are optional.
-
-    Example command-line use: python3 Main.py -t /usr/local/Cellar/tesseract/4.1.1/bin/tesseract
-
-    optional arguments:
-      -h, --help         show this help message and exit
-      -c  , --crop       crop OCR area in pixels (two vals required): width height
-      -v , --view_mode   view mode for OCR boxes display (default=1)
-      -sv, --show_views  show the available view modes and descriptions
-      -l , --language    code for tesseract language, use + to add multiple (ex: chi_sim+chi_tra)
-      -sl, --show_langs  show list of tesseract (4.0+) supported langs
-
-    required named arguments:
-      -t , --tess_path   path to the cmd root of tesseract install (see docs for further help)
+    A path to the Tesseract cmd root is required.
     """
     parser = argparse.ArgumentParser()
 
-    # Required:
     requiredNamed = parser.add_argument_group('required named arguments')
 
     requiredNamed.add_argument('-t', '--tess_path',
