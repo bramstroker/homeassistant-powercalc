f205732537af753aff187002bdd64ccb748a4432
@RubenKelevra <cyrond@gmail.com>
2022-06-24 17:08:40 +0200

    add Ikea Slagsida (#797)
    An ESPHome project powered by an ESP8266
    See https://community.home-assistant.io/t/howto-integrate-ikea-slagsida-under-cupboard-light/394689

--- custom_components/powercalc/aliases.py
+++ custom_components/powercalc/aliases.py
@@ -26,6 +26,7 @@ MODEL_DIRECTORY_MAPPING = {
     "IKEA of Sweden": {
         "FLOALT panel WS 30x30": "L1527",
         "FLOALT panel WS 60x60": "L1529",
+        "Slagsida": "L1616",
         "TRADFRI bulb E14 WS opal 400lm": "LED1536G5",
         "TRADFRI bulb GU10 WS 400lm": "LED1537R6",
         "TRADFRI bulb E27 WS opal 980lm": "LED1545G12",
