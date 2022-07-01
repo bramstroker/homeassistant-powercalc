b40aa47fb420fee63bc17faadd4679bad69eafc4
Bram <bgerritsen@emico.nl>
2022-07-01 10:38:16 +0200

    Allow to setup multiple power sensors for the same entity

--- custom_components/powercalc/const.py
+++ custom_components/powercalc/const.py
@@ -19,6 +19,7 @@ DATA_CALCULATOR_FACTORY = "calculator_factory"
 DATA_CONFIGURED_ENTITIES = "configured_entities"
 DATA_DISCOVERED_ENTITIES = "discovered_entities"
 DATA_DOMAIN_ENTITIES = "domain_entities"
+DATA_USED_UNIQUE_IDS = "used_unique_ids"
 
 DUMMY_ENTITY_ID = "sensor.dummy"
 
