697f6631ccdb602c6e5b2797e02ce394ab57b022
Bram <bgerritsen@emico.nl>
2022-07-01 13:47:03 +0200

    Commit WIP

--- custom_components/powercalc/config_flow.py
+++ custom_components/powercalc/config_flow.py
@@ -5,20 +5,31 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
-from homeassistant.core import callback
+from homeassistant.helpers import selector
 import homeassistant.helpers.config_validation as cv
 
-from .const import (  # pylint: disable=unused-import
-    CONF_LIGHTS,
+from .const import (
     DOMAIN,
-    EXTRA_VALIDATION,
-    NONE_STR,
-    VALIDATION_TUPLES,
+    CONF_MODE,
+    CONF_STANDBY_POWER,
+    CONF_DISABLE_STANDBY_POWER,
+    CALCULATION_MODES,
+    MODE_FIXED
 )
-from .switch import _supported_features
 
 _LOGGER = logging.getLogger(__name__)
 
+CONFIG_SCHEMA = vol.Schema({
+    vol.Required(CONF_NAME): str,
+    vol.Optional(CONF_MODE, default=MODE_FIXED): selector.SelectSelector(
+        selector.SelectSelectorConfig(
+            options=CALCULATION_MODES,
+            mode=selector.SelectSelectorMode.DROPDOWN,
+        )
+    ),
+    vol.Optional(CONF_STANDBY_POWER): vol.Coerce(float),
+    vol.Optional(CONF_DISABLE_STANDBY_POWER, default=False): cv.boolean,
+})
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Adaptive Lighting."""
@@ -27,82 +38,13 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        errors = {}
-
-        if user_input is not None:
-            await self.async_set_unique_id(user_input[CONF_NAME])
-            self._abort_if_unique_id_configured()
-            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
+        if not user_input:
+            return self.async_show_form(
+                step_id="user", data_schema=CONFIG_SCHEMA,
+            )
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({vol.Required(CONF_NAME): str}),
-            errors=errors,
-        )
-
-    # async def async_step_import(self, user_input=None):
-    #     """Handle configuration by yaml file."""
-    #     await self.async_set_unique_id(user_input[CONF_NAME])
-    #     for entry in self._async_current_entries():
-    #         if entry.unique_id == self.unique_id:
-    #             self.hass.config_entries.async_update_entry(entry, data=user_input)
-    #             self._abort_if_unique_id_configured()
-    #     return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
-
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
-        """Get the options flow for this handler."""
-        return OptionsFlowHandler(config_entry)
-
-
-def validate_options(user_input, errors):
-    """Validate the options in the OptionsFlow.
-    This is an extra validation step because the validators
-    in `EXTRA_VALIDATION` cannot be serialized to json.
-    """
-    for key, (validate, _) in EXTRA_VALIDATION.items():
-        # these are unserializable validators
-        value = user_input.get(key)
-        try:
-            if value is not None and value != NONE_STR:
-                validate(value)
-        except vol.Invalid:
-            _LOGGER.exception("Configuration option %s=%s is incorrect", key, value)
-            errors["base"] = "option_error"
-
-
-class OptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle a option flow for Adaptive Lighting."""
-
-    def __init__(self, config_entry: config_entries.ConfigEntry):
-        """Initialize options flow."""
-        self.config_entry = config_entry
-
-    async def async_step_init(self, user_input=None):
-        """Handle options flow."""
-        conf = self.config_entry
-        if conf.source == config_entries.SOURCE_IMPORT:
-            return self.async_show_form(step_id="init", data_schema=None)
-        errors = {}
-        if user_input is not None:
-            validate_options(user_input, errors)
-            if not errors:
-                return self.async_create_entry(title="", data=user_input)
-
-        all_lights = [
-            light
-            for light in self.hass.states.async_entity_ids("light")
-            if _supported_features(self.hass, light)
-        ]
-        to_replace = {CONF_LIGHTS: cv.multi_select(sorted(all_lights))}
-
-        options_schema = {}
-        for name, default, validation in VALIDATION_TUPLES:
-            key = vol.Optional(name, default=conf.options.get(name, default))
-            value = to_replace.get(name, validation)
-            options_schema[key] = value
-
-        return self.async_show_form(
-            step_id="init", data_schema=vol.Schema(options_schema), errors=errors
+            errors={},
         )
