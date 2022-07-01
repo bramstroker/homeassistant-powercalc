c1f2437c178fdefa78be2676b9b6755e89dfa396
Bram <bgerritsen@emico.nl>
2022-07-01 14:36:48 +0200

    Commit WIP

--- custom_components/powercalc/config_flow.py
+++ custom_components/powercalc/config_flow.py
@@ -4,23 +4,29 @@ import logging
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_NAME
+from homeassistant.const import CONF_ENTITY_ID, CONF_NAME, CONF_UNIQUE_ID
 from homeassistant.helpers import selector
 import homeassistant.helpers.config_validation as cv
 
+from .strategy.fixed import CONFIG_SCHEMA as FIXED_SCHEMA
+
 from .const import (
+    CONF_CREATE_UTILITY_METERS,
     DOMAIN,
     CONF_MODE,
     CONF_STANDBY_POWER,
     CONF_DISABLE_STANDBY_POWER,
     CALCULATION_MODES,
-    MODE_FIXED
+    MODE_FIXED,
+    CONF_CREATE_ENERGY_SENSOR
 )
 
 _LOGGER = logging.getLogger(__name__)
 
 CONFIG_SCHEMA = vol.Schema({
     vol.Required(CONF_NAME): str,
+    vol.Optional(CONF_UNIQUE_ID): cv.string,
+    vol.Required(CONF_ENTITY_ID): selector.EntitySelector(),
     vol.Optional(CONF_MODE, default=MODE_FIXED): selector.SelectSelector(
         selector.SelectSelectorConfig(
             options=CALCULATION_MODES,
@@ -29,6 +35,8 @@ CONFIG_SCHEMA = vol.Schema({
     ),
     vol.Optional(CONF_STANDBY_POWER): vol.Coerce(float),
     vol.Optional(CONF_DISABLE_STANDBY_POWER, default=False): cv.boolean,
+    vol.Optional(CONF_CREATE_ENERGY_SENSOR, default=True): cv.boolean,
+    vol.Optional(CONF_CREATE_UTILITY_METERS, default=True): cv.boolean
 })
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -43,8 +51,18 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 step_id="user", data_schema=CONFIG_SCHEMA,
             )
 
+        if user_input.get(CONF_MODE) == MODE_FIXED:
+            return await self.async_step_fixed(user_input)
+
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({vol.Required(CONF_NAME): str}),
+            errors={},
+        )
+    
+    async def async_step_fixed(self, user_input=None):
+        return self.async_show_form(
+            step_id="fixed",
+            data_schema=FIXED_SCHEMA,
             errors={},
         )
