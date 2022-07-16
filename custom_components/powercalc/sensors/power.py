2eb9fb4f61d711b4d85aaeacb2321a6c3c03cf72
Bram Gerritsen <bgerritsen@emico.nl>
2022-07-16 14:59:23 +0200

    Implement consistent naming pattern (name and friendly name) for all sensors (#846)

--- custom_components/powercalc/sensors/power.py
+++ custom_components/powercalc/sensors/power.py
@@ -32,6 +32,7 @@ from homeassistant.helpers.event import (
 from homeassistant.helpers.template import Template
 from homeassistant.helpers.typing import DiscoveryInfoType, HomeAssistantType
 
+from .abstract import generate_power_sensor_name, generate_power_sensor_entity_id
 from custom_components.powercalc.common import SourceEntity
 from custom_components.powercalc.const import (
     ATTR_CALCULATION_MODE,
@@ -75,8 +76,6 @@ from custom_components.powercalc.strategy.strategy_interface import (
     PowerCalculationStrategyInterface,
 )
 
-ENTITY_ID_FORMAT = SENSOR_DOMAIN + ".{}"
-
 _LOGGER = logging.getLogger(__name__)
 
 
@@ -105,23 +104,13 @@ async def create_virtual_power_sensor(
 ) -> VirtualPowerSensor:
     """Create the power sensor entity"""
 
-    name_pattern = sensor_config.get(CONF_POWER_SENSOR_NAMING)
-    name = sensor_config.get(CONF_NAME) or source_entity.name
-    if CONF_POWER_SENSOR_FRIENDLY_NAMING in sensor_config:
-        friendly_name_pattern = sensor_config.get(CONF_POWER_SENSOR_FRIENDLY_NAMING)
-        name = friendly_name_pattern.format(name)
-    else:
-        name = name_pattern.format(name)
-
-    object_id = sensor_config.get(CONF_NAME) or source_entity.object_id
+    name = generate_power_sensor_name(sensor_config, sensor_config.get(CONF_NAME), source_entity)
+    entity_id = generate_power_sensor_entity_id(hass, sensor_config, source_entity)
     entity_category = sensor_config.get(CONF_POWER_SENSOR_CATEGORY)
-    entity_id = async_generate_entity_id(
-        ENTITY_ID_FORMAT, name_pattern.format(object_id), hass=hass
-    )
 
     unique_id = sensor_config.get(CONF_UNIQUE_ID) or source_entity.unique_id
     if unique_id:
-        async_migrate_entity_id(hass, SENSOR_DOMAIN, unique_id, entity_id)
+        async_migrate_entity_id(hass, SENSOR_DOMAIN, unique_id=unique_id, new_entity_id=entity_id)
 
     light_model = None
     try:
