fb9f0dce5d3d0b92087469df9c3d9fb9907ba134
Bram Gerritsen <bgerritsen@emico.nl>
2022-07-21 11:08:53 +0200

    Allow to include previously configured entity in a group later (#877)

--- custom_components/powercalc/errors.py
+++ custom_components/powercalc/errors.py
@@ -13,10 +13,14 @@ class SensorConfigurationError(PowercalcSetupError):
 class SensorAlreadyConfiguredError(SensorConfigurationError):
     """Raised when power sensors has already been configured before for the entity"""
 
-    def __init__(self, source_entity_id: str):
+    def __init__(self, source_entity_id: str, existing_entities: list = []):
+        self.existing_entities = existing_entities
         super().__init__(
             f"{source_entity_id}: This entity has already configured a power sensor. When you want to configure it twice make sure to give it a unique_id"
         )
+    
+    def get_existing_entities(self) -> list:
+        return self.existing_entities
 
 
 class StrategyConfigurationError(PowercalcSetupError):
