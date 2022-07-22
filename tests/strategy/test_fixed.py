21e452f508f1953a5178b196e561787ef62ecbc4
Bram <bgerritsen@emico.nl>
2022-07-22 17:31:53 +0200

    Add fixed strategy tests

---
+++ tests/strategy/test_fixed.py
@@ -0,0 +1,62 @@
+from homeassistant.setup import async_setup_component
+from homeassistant.core import State
+from homeassistant.const import (
+    STATE_ON,
+)
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.template import Template
+from custom_components.powercalc.common import SourceEntity
+from homeassistant.components import (
+    input_number
+)
+
+from custom_components.powercalc.strategy.fixed import FixedStrategy
+
+async def test_simple_power():
+    source_entity = SourceEntity(
+        "test",
+        "switch.test",
+        "switch"
+    )
+    strategy = FixedStrategy(source_entity, power=50, per_state_power=None)
+    assert 50 == await strategy.calculate(State("switch.test", STATE_ON))
+
+async def test_template_power(hass: HomeAssistant):
+    assert await async_setup_component(
+        hass, input_number.DOMAIN, {"input_number": {"test": {"min": "0", "max": "100", "initial": "42"}}}
+    )
+    
+    await hass.async_block_till_done()
+
+    source_entity = SourceEntity(
+        "test",
+        "switch.test",
+        "switch"
+    )
+    strategy = FixedStrategy(
+        source_entity,
+        power=Template("{{states('input_number.test')}}", hass),
+        per_state_power=None)
+
+    assert 42 == await strategy.calculate(State("switch.test", STATE_ON))
+
+async def test_states_power(hass: HomeAssistant):
+    entity_id = "media_player.sonos_living"
+    source_entity = SourceEntity(
+        "sonos_living",
+        entity_id,
+        "media_player"
+    )
+    strategy = FixedStrategy(
+        source_entity,
+        power=20,
+        per_state_power={
+            "playing": 8.3,
+            "paused": 2.25,
+            "idle": 1.5
+        }
+    )
+    assert 8.3 == await strategy.calculate(State(entity_id, "playing"))
+    assert 2.25 == await strategy.calculate(State(entity_id, "paused"))
+    assert 1.5 == await strategy.calculate(State(entity_id, "idle"))
+    assert 20 == await strategy.calculate(State(entity_id, "whatever"))
