646702b9bae8faa5594cdbde9fd9ba498b211b07
Bram <bgerritsen@emico.nl>
2022-07-22 22:06:44 +0200

    Add test for group sensors

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,5 +1,13 @@
-from homeassistant.core import HomeAssistant
-from homeassistant.const import CONF_PLATFORM, CONF_ENTITY_ID, ATTR_ENTITY_ID, SERVICE_TURN_ON
+from homeassistant.core import HomeAssistant, CoreState
+from homeassistant.const import (
+    CONF_PLATFORM,
+    CONF_ENTITY_ID,
+    ATTR_ENTITY_ID,
+    SERVICE_TURN_ON,
+    CONF_ENTITIES,
+    CONF_UNIQUE_ID,
+    STATE_ON
+)
 from homeassistant.setup import async_setup_component
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
 from homeassistant.components.light import DOMAIN as LIGHT_DOMAIN
@@ -7,9 +15,10 @@ from homeassistant.components import (
     input_boolean,
     sensor
 )
+from homeassistant.helpers.entity_platform import split_entity_id
 
 from pytest_homeassistant_custom_component.common import mock_entity_platform
-from custom_components.powercalc.const import CONF_FIXED, CONF_MODE, CONF_POWER, DOMAIN, CalculationStrategy
+from custom_components.powercalc.const import ATTR_ENTITIES, CONF_CREATE_GROUP, CONF_FIXED, CONF_MODE, CONF_POWER, DOMAIN, CalculationStrategy
 
 async def test_fixed_power_sensor_from_yaml(hass: HomeAssistant, enable_custom_integrations):
     source_entity = "input_boolean.test"
@@ -36,12 +45,83 @@ async def test_fixed_power_sensor_from_yaml(hass: HomeAssistant, enable_custom_i
     state = hass.states.get("sensor.test_power")
     assert state.state == "0.00"
 
-    await hass.services.async_call(
-        input_boolean.DOMAIN,
-        SERVICE_TURN_ON,
-        {ATTR_ENTITY_ID: source_entity},
-        blocking=True,
-    )
+    hass.states.async_set(source_entity, STATE_ON)
+    await hass.async_block_till_done()
 
     state = hass.states.get("sensor.test_power")
     assert state.state == "50.00"
+
+    assert hass.states.get("sensor.test_energy")
+
+async def test_create_nested_group_sensor(hass: HomeAssistant):
+    assert await async_setup_component(
+        hass, input_boolean.DOMAIN, {"input_boolean": {"test": None}}
+    )
+    assert await async_setup_component(
+        hass, input_boolean.DOMAIN, {"input_boolean": {"test1": None}}
+    )
+    assert await async_setup_component(
+        hass, input_boolean.DOMAIN, {"input_boolean": {"test2": None}}
+    )
+
+    await hass.async_block_till_done()
+
+    await async_setup_component(
+        hass,
+        sensor.DOMAIN,
+        {
+            sensor.DOMAIN: {
+                CONF_PLATFORM: DOMAIN,
+                CONF_CREATE_GROUP: "TestGroup1",
+                CONF_ENTITIES: [
+                    {
+                        CONF_ENTITY_ID: "input_boolean.test",
+                        CONF_MODE: CalculationStrategy.FIXED,
+                        CONF_FIXED: {
+                            CONF_POWER: 50
+                        }
+                    },
+                    {
+                        CONF_ENTITY_ID: "input_boolean.test1", 
+                        CONF_MODE: CalculationStrategy.FIXED,
+                        CONF_FIXED: {
+                            CONF_POWER: 50
+                        }
+                    },
+                    {
+                        CONF_CREATE_GROUP: "TestGroup2",
+                        CONF_ENTITIES: [
+                            {
+                                CONF_ENTITY_ID: "input_boolean.test2", 
+                                CONF_MODE: CalculationStrategy.FIXED,
+                                CONF_FIXED: {
+                                    CONF_POWER: 50
+                                }
+                            },
+                        ]
+                    }
+                ]
+            }
+        }
+    )
+    await hass.async_block_till_done()
+
+    hass.states.async_set("input_boolean.test", STATE_ON)
+    hass.states.async_set("input_boolean.test1", STATE_ON)
+    hass.states.async_set("input_boolean.test2", STATE_ON)
+
+    await hass.async_block_till_done()
+
+    group1 = hass.states.get("sensor.testgroup1_power")
+    assert group1.attributes[ATTR_ENTITIES] == {
+        "sensor.test_power",
+        "sensor.test1_power",
+        "sensor.test2_power",
+    }
+    assert group1.state == "150.00"
+
+    group2 = hass.states.get("sensor.testgroup2_power")
+    assert group2.attributes[ATTR_ENTITIES] == {
+        "sensor.test2_power",
+    }
+    assert group2.state == "50.00"
