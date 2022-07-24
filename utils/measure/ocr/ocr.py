93adc9518a72f4d9fa36aa1bb311e707f00b0770
Bram <bgerritsen@emico.nl>
2022-07-24 15:00:53 +0200

    Remove commented code

--- utils/measure/ocr/ocr.py
+++ utils/measure/ocr/ocr.py
@@ -256,7 +256,6 @@ class OCR:
 
                     #Convert to grayscale for easier OCR detection
                     frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-                    # frame = cv2.adaptiveThreshold(frame, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
                     
                     match = pytesseract.image_to_string(
                         frame,
