045170aef9e0832f27f86b62ba2db8479b1372da
Bram <bgerritsen@emico.nl>
2022-07-24 14:08:21 +0200

    CS fixes

--- custom_components/powercalc/sensors/utility_meter.py
+++ custom_components/powercalc/sensors/utility_meter.py
@@ -54,7 +54,7 @@ async def create_utility_meters(
 
     utility_meters = []
 
-    if not DATA_UTILITY in hass.data:
+    if DATA_UTILITY not in hass.data:
         hass.data[DATA_UTILITY] = {}
 
     tariffs = sensor_config.get(CONF_UTILITY_METER_TARIFFS)
