1704291822c7bec4ec828a74d2df97f6067c02a5
Bram <bgerritsen@emico.nl>
2022-07-25 18:42:09 +0200

    Add test for subgroups

--- tests/sensors/test_group.py
+++ tests/sensors/test_group.py
@@ -5,24 +5,32 @@ from homeassistant.const import (
     ATTR_UNIT_OF_MEASUREMENT,
     CONF_ENTITIES,
     CONF_ENTITY_ID,
+    CONF_NAME,
     CONF_PLATFORM,
+    CONF_UNIQUE_ID,
     STATE_ON,
     ENERGY_KILO_WATT_HOUR,
     POWER_WATT,
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.setup import async_setup_component
-from pytest_homeassistant_custom_component.common import MockEntity, MockEntityPlatform
+from pytest_homeassistant_custom_component.common import (
+    MockEntity, MockEntityPlatform, MockConfigEntry
+)
 
 from custom_components.powercalc.const import (
     ATTR_ENTITIES,
     CONF_CREATE_GROUP,
     CONF_FIXED,
+    CONF_GROUP_POWER_ENTITIES,
     CONF_MODE,
     CONF_POWER,
     CONF_POWER_SENSOR_ID,
+    CONF_SENSOR_TYPE,
+    CONF_SUB_GROUPS,
     DOMAIN,
     CalculationStrategy,
+    SensorType,
 )
 
 
@@ -46,6 +54,7 @@ async def test_grouped_power_sensor(hass: HomeAssistant):
                 CONF_ENTITIES: [
                     {
                         CONF_ENTITY_ID: "input_boolean.test1",
+                        CONF_UNIQUE_ID: "4312393932",
                         CONF_MODE: CalculationStrategy.FIXED,
                         CONF_FIXED: {CONF_POWER: 10.5},
                     },
@@ -83,4 +92,49 @@ async def test_grouped_power_sensor(hass: HomeAssistant):
     assert energy_state.attributes.get(ATTR_ENTITIES) == {
         "sensor.test1_energy",
         "sensor.test2_energy",
+    }
+
+async def test_subgroups_from_config_entry(hass: HomeAssistant):
+    config_entry_groupa = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            CONF_SENSOR_TYPE: SensorType.GROUP,
+            CONF_NAME: "GroupA",
+            CONF_GROUP_POWER_ENTITIES: [
+                "sensor.test1_power"
+            ]
+        }
+    )
+    config_entry_groupa.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(config_entry_groupa.entry_id)
+    await hass.async_block_till_done()
+
+    config_entry_groupb = MockConfigEntry(
+        domain=DOMAIN,
+        data={
+            CONF_SENSOR_TYPE: SensorType.GROUP,
+            CONF_NAME: "GroupB",
+            CONF_GROUP_POWER_ENTITIES: [
+                "sensor.test2_power"
+            ],
+            CONF_SUB_GROUPS: [
+                config_entry_groupa.entry_id,
+                "464354354543" # Non existing entry_id, should not break setup
+            ]
+        }
+    )
+    config_entry_groupb.add_to_hass(hass)
+    assert await hass.config_entries.async_setup(config_entry_groupb.entry_id)
+    await hass.async_block_till_done()
+
+    groupa_state = hass.states.get("sensor.groupa_power")
+    assert groupa_state
+    assert groupa_state.attributes.get(ATTR_ENTITIES) == {
+        "sensor.test1_power",
+    }
+    groupb_state = hass.states.get("sensor.groupb_power")
+    assert groupb_state
+    assert groupb_state.attributes.get(ATTR_ENTITIES) == {
+        "sensor.test1_power",
+        "sensor.test2_power",
     }
