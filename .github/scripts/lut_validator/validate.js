dc2ca919ec076d17ffda61652993442ac6fcde40
Bram Gerritsen <bgerritsen@emico.nl>
2022-09-20 12:12:08 +0100

    Remove files for impossible color modes and improve lut validator (#1090)

--- .github/scripts/lut_validator/validate.js
+++ .github/scripts/lut_validator/validate.js
@@ -1,44 +1,80 @@
-const csval = require('csval');
-const readdirp = require('readdirp');
-const { ungzip } = require('node-gzip');
-const fs = require('fs');
-const { exit } = require('process');
-const path = require('path');
+import csval from 'csval';
+import readdirp from 'readdirp';
+import zlib from 'zlib';
+import fs from 'fs';
+import process from 'process';
+import path from 'path';
+import chalk from 'chalk';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import util from 'util';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const validColorModeCombinations = [
+    'color_temp,hs',
+    'color_temp',
+    'hs',
+    'brightness'
+]
 
 const main = async () => {
-  const dataDirectory = path.join(__dirname, '../../../custom_components/powercalc/data')
-  let hasError = false
-  for await (const file of readdirp(dataDirectory, {fileFilter: '*.csv.gz'})) {
-    const colorMode = file.basename.substring(0, file.basename.indexOf('.csv.gz'))
+    const dataDirectory = path.join(__dirname, '../../../custom_components/powercalc/data')
+    let hasError = false
+    for await (const model_dir of readdirp(dataDirectory, {depth: 2, type: 'directories'})) {
+        if (!model_dir.path.includes('/')) {
+            continue
+        }
+        console.log('Processing model directory ' + model_dir.path)
+        let colorModes = new Set()
+        for await (const file of readdirp(model_dir.fullPath, {fileFilter: '*.csv.gz'})) {
+            const colorMode = file.basename.substring(0, file.basename.indexOf('.csv.gz'))
+            colorModes.add(colorMode)
+            console.log('Checking ' + file.path)
+            const gzipped = fs.readFileSync(file.fullPath)
+            const csvBuffer = await util.promisify(zlib.gunzip)(gzipped)
+            const parsed = await csval.parseCsv(csvBuffer.toString());
+            const rules = await csval.readRules(path.join(__dirname, 'rules/' + colorMode + '.json'));
+            try {
+                await csval.validate(parsed, rules)
+                validateMaxBrightness(parsed)
+            } catch (ex) {
+                console.log(chalk.red('Invalid'))
+                console.log(chalk.red(ex.message))
+                hasError = true
+                continue;
+            }
+            console.log(chalk.green('Valid'))
+        }
 
-    console.log('Checking ' + file.path)
-    const gzipped = fs.readFileSync(file.fullPath)
-    const csvBuffer = await ungzip(gzipped)
-    const parsed = await csval.parseCsv(csvBuffer.toString());
-    const rules = await csval.readRules(path.join(__dirname, 'rules/' + colorMode + '.json'));
-    try {
-      await csval.validate(parsed, rules)
-      validateMaxBrightness(parsed)
-    } catch (ex) {
-      console.log('Invalid')
-      console.log(ex)
-      hasError = true
-      continue;
+        if (colorModes.size > 0) {
+            try {
+                validateColorModes(colorModes)
+            } catch (ex) {
+                console.log(chalk.red(ex))
+                hasError = true
+            }
+        }
     }
-    console.log('Valid')
-  }
 
-  if (hasError) {
-    exit(1)
-  }
+    if (hasError) {
+        process.exit(1)
+    }
 };
 
 const validateMaxBrightness = (parsedCsv) => {
-  brightnessValues = parsedCsv['data'].map((row) => row['bri'])
-  maxBrightness = Math.max(...brightnessValues)
-  if (maxBrightness < 250) {
-    throw new Error('Max brightness level ' + maxBrightness + ' is less than 250. Measurements probably not finished completely')
-  }
+    const brightnessValues = parsedCsv['data'].map((row) => row['bri'])
+    const maxBrightness = Math.max(...brightnessValues)
+    if (maxBrightness < 250) {
+        throw new Error('Max brightness level ' + maxBrightness + ' is less than 250. Measurements probably not finished completely')
+    }
+}
+
+const validateColorModes = (colorModes) => {
+    if (validColorModeCombinations.indexOf([...colorModes].join(',')) === -1) {
+        throw new Error('Invalid color mode combination ' + [...colorModes].join(','))
+    }
 }
 
 main();
