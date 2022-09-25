6b5d59bddf76ad3f1a87664ef47f13111ef5a07e
Bram Gerritsen <bgerritsen@emico.nl>
2022-09-25 09:56:24 +0200

    Use forward entry setup (#1124)

--- custom_components/powercalc/__init__.py
+++ custom_components/powercalc/__init__.py
@@ -219,7 +219,7 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Powercalc integration from a config entry."""
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     entry.async_on_unload(entry.add_update_listener(async_update_entry))
     return True
 
