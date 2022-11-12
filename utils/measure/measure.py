8d28f271a9c9d2c4b3f8253ce7bcafe3d6e120ed
Thorjan Knudsvik <t@knudsvik.com>
2022-11-12 09:32:02 +0100

    fix whitespace
    Co-authored-by: Bram Gerritsen <bgerritsen@gmail.com>

--- utils/measure/measure.py
+++ utils/measure/measure.py
@@ -951,7 +951,7 @@ def main():
             print(f'Prepare to start measuring the power for {duration} seconds on each volume level starting with 10 until 100 (with steps of 10 between)')
             print('Recommend to stream Pink Sound from https://www.genelec.com/audio-test-signals')
 
-            for volume in range(10,101, 10):
+            for volume in range(10, 101, 10):
                 if inquirer.confirm(f'Set volume to {volume}% and confirm to start next {duration} second measurement'):
                     summary[volume] = measure.measure_average(duration)
             print('Summary of all average measurements:')
