4dd5b140f96c227fb8d3f1af1a446a3024ac9544
Bram Gerritsen <bgerritsen@emico.nl>
2022-12-29 12:35:59 +0100

    Potential fix for does not generate unique IDs error (#1390)

--- tests/sensors/test_utility_meter.py
+++ tests/sensors/test_utility_meter.py
@@ -1,8 +1,12 @@
-from homeassistant.components import input_boolean, sensor, utility_meter
+import logging
+
+import pytest
+from homeassistant.components import sensor, utility_meter
 from homeassistant.components.utility_meter.sensor import (
     ATTR_SOURCE_ID,
     ATTR_STATUS,
     ATTR_TARIFF,
+    CONF_UNIQUE_ID,
     COLLECTING,
     PAUSED,
 )
@@ -12,16 +16,21 @@ from homeassistant.setup import async_setup_component
 
 from custom_components.powercalc.const import (
     CONF_CREATE_UTILITY_METERS,
+    CONF_ENERGY_SENSOR_ID,
     CONF_FIXED,
     CONF_MODE,
     CONF_POWER,
+    CONF_POWER_SENSOR_ID,
     CONF_UTILITY_METER_TARIFFS,
     CONF_UTILITY_METER_TYPES,
     DOMAIN,
+    DUMMY_ENTITY_ID,
     CalculationStrategy,
 )
+from homeassistant.helpers.entity_registry import RegistryEntry
+from pytest_homeassistant_custom_component.common import mock_registry
 
-from ..common import create_input_boolean
+from ..common import create_input_boolean, run_powercalc_setup
 
 
 async def test_tariff_sensors_are_created(hass: HomeAssistant):
@@ -65,3 +74,61 @@ async def test_tariff_sensors_are_created(hass: HomeAssistant):
     assert offpeak_sensor.attributes[ATTR_SOURCE_ID] == "sensor.test_energy"
     assert offpeak_sensor.attributes[ATTR_TARIFF] == "offpeak"
     assert offpeak_sensor.attributes[ATTR_STATUS] == PAUSED
+
+
+async def test_utility_meter_is_not_created_twice(hass: HomeAssistant, caplog: pytest.LogCaptureFixture) -> None:
+    caplog.set_level(logging.ERROR)
+    power_sensor_id = "sensor.test_power"
+    energy_sensor_id = "sensor.test_energy"
+    utility_meter_id = "sensor.test_energy_daily"
+    entity_registry = mock_registry(
+        hass,
+        {
+            power_sensor_id: RegistryEntry(
+                entity_id=power_sensor_id,
+                unique_id="1234",
+                name="Test power",
+                platform="powercalc",
+            ),
+            energy_sensor_id: RegistryEntry(
+                entity_id=energy_sensor_id,
+                unique_id="1234_energy",
+                name="Test energy",
+                platform="powercalc",
+            ),
+            utility_meter_id: RegistryEntry(
+                entity_id=utility_meter_id,
+                unique_id="1234_energy_daily",
+                name="Test energy daily",
+                platform="powercalc",
+            ),
+        },
+    )
+
+    await run_powercalc_setup(
+        hass,
+        {
+            CONF_ENTITY_ID: DUMMY_ENTITY_ID,
+            CONF_UNIQUE_ID: "1234",
+            CONF_CREATE_UTILITY_METERS: True,
+            CONF_UTILITY_METER_TYPES: ["daily"],
+            CONF_POWER_SENSOR_ID: power_sensor_id,
+            CONF_ENERGY_SENSOR_ID: energy_sensor_id
+        },
+    )
+
+    await run_powercalc_setup(
+        hass,
+        {
+            CONF_ENTITY_ID: DUMMY_ENTITY_ID,
+            CONF_UNIQUE_ID: "1234",
+            CONF_CREATE_UTILITY_METERS: True,
+            CONF_UTILITY_METER_TYPES: ["daily"],
+            CONF_POWER_SENSOR_ID: power_sensor_id,
+            CONF_ENERGY_SENSOR_ID: energy_sensor_id
+        },
+    )
+
+    assert entity_registry.async_get(utility_meter_id)
+    assert hass.states.get(utility_meter_id)
+    assert len(caplog.records) == 0
