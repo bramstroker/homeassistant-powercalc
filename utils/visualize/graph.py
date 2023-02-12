bbea6bf9987ce57ef4f292b82fb8253952e9efce
Bram <bgerritsen@emico.nl>
2023-02-12 09:31:01 +0100

    Marker size

--- utils/visualize/graph.py
+++ utils/visualize/graph.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #df1 = pd.read_csv("LTG002.csv")
-df1 = pd.read_csv("LCB001.csv")
-df2 = pd.read_csv("ct.csv")
+df1 = pd.read_csv("color_temp2.csv")
+df2 = pd.read_csv("color_temp.csv")
 #df2 = pd.read_csv("miliskin.csv")
 
 def plot_data(df, color):
     x = df['bri']
     y = df['watt']
-    plt.scatter(x, y, color=color)
+    plt.scatter(x, y, color=color, marker=".", s=10)
 
 plot_data(df1, 'blue')
 plot_data(df2, 'red')
 
-plt.legend(['LTG002', 'Miliskin'])
+plt.legend(['old', 'new'])
 plt.show()
