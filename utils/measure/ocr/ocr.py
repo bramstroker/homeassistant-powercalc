c67deea1d05c2652b300810f60fb8a6da2f5ce2e
Bram <bgerritsen@emico.nl>
2023-02-18 11:16:13 +0100

    Fix linting

--- utils/measure/ocr/ocr.py
+++ utils/measure/ocr/ocr.py
@@ -263,7 +263,7 @@ class OcrRegionSelection:
 
     def get_cropped_frame(self, frame: numpy.ndarray) -> numpy.ndarray:
         return frame[
-            self.selection[1] : self.selection[3],
+            self.selection[1] : self.selection[3],  # noqa E203
             self.selection[0] : self.selection[2],  # noqa E203
         ]
 
