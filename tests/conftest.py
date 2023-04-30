90cc64cb888f4a08c204f66457c55c26b9aa7696
Bram <bgerritsen@emico.nl>
2023-04-30 09:36:10 +0200

    Upgrade HA for the tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,6 +24,17 @@ def auto_enable_custom_integrations(enable_custom_integrations):
     yield
 
 
+@pytest.fixture(autouse=True)
+def expected_lingering_timers() -> bool:
+    """Temporary ability to bypass test failures.
+    Parametrize to True to bypass the pytest failure.
+    @pytest.mark.parametrize("expected_lingering_timers", [True])
+    This should be removed when all lingering timers have been cleaned up.
+    See https://github.com/MatthewFlamm/pytest-homeassistant-custom-component/issues/153
+    """
+    return True
+
+
 @pytest.fixture
 def enable_custom_integrations(hass):
     """Enable custom integrations defined in the test dir."""
