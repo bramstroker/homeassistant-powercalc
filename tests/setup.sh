9c349e0554eea88b80dfd30e345d7d4ced3684c3
Bram <bgerritsen@emico.nl>
2023-06-17 13:59:49 +0200

    Fix workflows

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,8 +1,4 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cd "$SCRIPTPATH/.."
-pip install poetry
-poetry install
-
 cd "$SCRIPTPATH/../custom_components"
 ln -sf ../tests/testing_config/custom_components/test test
