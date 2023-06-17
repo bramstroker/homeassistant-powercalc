908174f6586eae9e0399b25eee76ebb197fc2a31
Bram <bgerritsen@emico.nl>
2023-06-17 13:41:42 +0200

    Fix mypy

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,6 +1,8 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-python3 -m pip install -r ${SCRIPTPATH}/../requirements.test.txt
+cd "$SCRIPTPATH/.."
+pip install poetry
+poetry install
 
 cd "$SCRIPTPATH/../custom_components"
 ln -sf ../tests/testing_config/custom_components/test test
