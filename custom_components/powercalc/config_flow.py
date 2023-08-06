5a8d688d0828bf6618abfb0ae1c2101eb353b188
Bram <bgerritsen@emico.nl>
2023-08-06 10:06:00 +0200

    Support templates for standby power

--- custom_components/powercalc/config_flow.py
+++ custom_components/powercalc/config_flow.py
@@ -527,7 +527,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return await self.async_step_manufacturer()
 
     async def async_step_real_power(
-        self, user_input: dict[str, Any] | None = None
+        self, user_input: dict[str, Any] | None = None,
     ) -> FlowResult:
         """Handle the flow for real power sensor"""
 
