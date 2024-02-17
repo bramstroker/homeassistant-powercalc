b0e8524de0b9d7068ed005daf9c015a15c37f720
Bram Gerritsen <bgerritsen@emico.nl>
2024-02-17 12:43:03 +0100

    Rework device binding code to be more consise (#2058)

--- custom_components/powercalc/sensor.py
+++ custom_components/powercalc/sensor.py
@@ -10,7 +10,6 @@ from datetime import timedelta
 from typing import Any
 
 import homeassistant.helpers.config_validation as cv
-import homeassistant.helpers.device_registry as dr
 import homeassistant.helpers.entity_registry as er
 import voluptuous as vol
 from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
@@ -20,7 +19,6 @@ from homeassistant.components.utility_meter.const import METER_TYPES
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     CONF_CONDITION,
-    CONF_DEVICE,
     CONF_DOMAIN,
     CONF_ENTITIES,
     CONF_ENTITY_ID,
@@ -28,7 +26,7 @@ from homeassistant.const import (
     CONF_UNIQUE_ID,
 )
 from homeassistant.core import Event, HomeAssistant, callback
-from homeassistant.helpers import device_registry, entity_platform
+from homeassistant.helpers import entity_platform
 from homeassistant.helpers.entity import Entity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.entity_registry import (
@@ -123,13 +121,13 @@ from .const import (
     SensorType,
     UnitPrefix,
 )
+from .device_binding import attach_entities_to_source_device, bind_config_entry_to_device
 from .errors import (
     PowercalcSetupError,
     SensorAlreadyConfiguredError,
     SensorConfigurationError,
 )
 from .group_include.include import resolve_include_entities
-from .sensors.abstract import BaseEntity
 from .sensors.daily_energy import (
     DAILY_FIXED_ENERGY_SCHEMA,
     create_daily_fixed_energy_power_sensor,
@@ -808,33 +806,6 @@ async def create_individual_sensors(
     return EntitiesBucket(new=entities_to_add, existing=[])
 
 
-async def attach_entities_to_source_device(
-    config_entry: ConfigEntry | None,
-    entities_to_add: list[Entity],
-    hass: HomeAssistant,
-    source_entity: SourceEntity,
-) -> None:
-    """Set the entity to same device as the source entity, if any available."""
-    if source_entity.entity_entry and source_entity.device_entry:
-        device_id = source_entity.device_entry.id
-        device_registry = dr.async_get(hass)
-        for entity in (
-            entity for entity in entities_to_add if isinstance(entity, BaseEntity)
-        ):
-            try:
-                entity.source_device_id = source_entity.device_entry.id  # type: ignore
-            except AttributeError:  # pragma: no cover
-                _LOGGER.error("%s: Cannot set device id on entity", entity.entity_id)
-        if (
-            config_entry
-            and config_entry.entry_id not in source_entity.device_entry.config_entries
-        ):
-            device_registry.async_update_device(
-                device_id,
-                add_config_entry_id=config_entry.entry_id,
-            )
-
-
 async def check_entity_not_already_configured(
     sensor_config: dict,
     source_entity: SourceEntity,
@@ -857,24 +828,6 @@ async def check_entity_not_already_configured(
     entity_id = source_entity.entity_id
     if unique_id is None and entity_id in configured_entities:
         raise SensorAlreadyConfiguredError(source_entity.entity_id, existing_entities)
-
-
-def bind_config_entry_to_device(hass: HomeAssistant, config_entry: ConfigEntry) -> None:
-    """
-    When the user selected a specific device in the config flow, bind the config entry to that device
-    This will let HA bind all the powercalc entities for that config entry to the concerning device
-    """
-    device_id = config_entry.data.get(CONF_DEVICE)
-    if not device_id:
-        return
-
-    device_reg = device_registry.async_get(hass)
-    device_entry = device_reg.async_get(device_id)
-    if device_entry and config_entry.entry_id not in device_entry.config_entries:
-        device_reg.async_update_device(
-            device_id,
-            add_config_entry_id=config_entry.entry_id,
-        )
 
 
 @dataclass
