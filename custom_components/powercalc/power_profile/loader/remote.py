6bf9856881818d54a5961e1d6ee7ac82490a38e8
Bram <bgerritsen@emico.nl>
2024-04-01 14:48:03 +0200

    Commit WIP

--- custom_components/powercalc/power_profile/loader/remote.py
+++ custom_components/powercalc/power_profile/loader/remote.py
@@ -13,6 +13,9 @@ from custom_components.powercalc.power_profile.power_profile import DeviceType
 
 _LOGGER = logging.getLogger(__name__)
 
+DOWNLOAD_PROXY = "http://localhost:3000"
+ENDPOINT_LIBRARY = f"{DOWNLOAD_PROXY}/library"
+ENDPOINT_DOWNLOAD = f"{DOWNLOAD_PROXY}/download"
 
 class RemoteLoader(Loader):
     def __init__(self, hass: HomeAssistant) -> None:
@@ -22,12 +25,7 @@ class RemoteLoader(Loader):
         self.manufacturer_models: dict[str, list[dict]] = {}
 
     async def initialize(self) -> None:
-        # Todo: async io
-        with open(get_library_path("library.json")) as f:
-            self.library_contents = json.load(f)
-
-        if not self.library_contents:
-            raise LibraryLoadingError("Library.json JSON invalid")
+        await self.load_library_json()
 
         # Load contents of library JSON into memory
         manufacturers: list[dict] = self.library_contents.get("manufacturers", [])
@@ -40,6 +38,19 @@ class RemoteLoader(Loader):
                 if manufacturer_name not in self.manufacturer_models:
                     self.manufacturer_models[manufacturer_name] = []
                 self.manufacturer_models[manufacturer_name].append(model)
+
+    async def load_library_json(self) -> None:
+        """Load library.json file"""
+        async with aiohttp.ClientSession() as session:
+            async with session.get(ENDPOINT_LIBRARY) as resp:
+                if resp.status != 200:
+                    _LOGGER.debug("Failed to download library.json from github, falling back to local copy")
+                    with open(get_library_path("library.json")) as f:
+                        self.library_contents = json.load(f)
+                self.library_contents = await resp.json()
+
+        if not self.library_contents:
+            raise LibraryLoadingError("Library.json JSON invalid")
 
     async def get_manufacturer_listing(self, device_type: DeviceType | None) -> set[str]:
         """Get listing of available manufacturers."""
@@ -56,7 +67,7 @@ class RemoteLoader(Loader):
         return {
             model["id"] for model
             in self.manufacturer_models.get(manufacturer, [])
-            if not device_type or device_type in model.get("device_types", [])
+            if not device_type or device_type in model.get("device_type", DeviceType.LIGHT)
         }
 
     async def load_model(self, manufacturer: str, model: str, directory: str | None) -> tuple[dict, str] | None:
@@ -82,7 +93,8 @@ class RemoteLoader(Loader):
         #     if remote_modification_time > local_modification_time:
         #         _LOGGER.debug("Remote profile is newer than local profile")
         #         needs_update = True
-
+        #
+        # needs_update = True
         if needs_update:
             await self._download_profile(manufacturer, model, storage_path)
 
@@ -115,10 +127,9 @@ class RemoteLoader(Loader):
 
         _LOGGER.info("Downloading profile: %s/%s from github", manufacturer, model)
 
-        uri = f"http://localhost:3000/?manufacturer={manufacturer}&model={model}"
-
+        endpoint = f"{ENDPOINT_DOWNLOAD}/{manufacturer}/{model}"
         async with aiohttp.ClientSession() as session:
-            async with session.get(uri) as resp:
+            async with session.get(endpoint) as resp:
                 if resp.status != 200:
                     raise ProfileDownloadError(f"Failed to download profile: {manufacturer}/{model}")
                 resources = json.loads(await resp.read())
@@ -128,7 +139,9 @@ class RemoteLoader(Loader):
         # Download the files
         async with aiohttp.ClientSession() as session:
             for resource in resources:
-                async with session.get(resource.url) as resp:
-                    with open(os.path.join(storage_path, resource.filename), "wb") as f:
+                async with session.get(resource["url"]) as resp:
+                    path = os.path.join(storage_path, resource["path"])
+                    os.makedirs(os.path.dirname(path), exist_ok=True)
+                    with open(path, "wb") as f:
                         f.write(await resp.read())
 
--- utils/library_downloader/src/index.ts
+++ utils/library_downloader/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import apicache from "apicache";
 import { Octokit } from '@octokit/rest';
 
 dotenv.config();
@@ -10,39 +11,103 @@ const githubToken = process.env.GITHUB_TOKEN;
 const owner = "bramstroker"
 const repo = "homeassistant-powercalc"
 
+apicache.options(
+    {
+        statusCodes: {
+            include: [200],
+          },
+    }
+)
 
-app.get("/", async (req: Request, res: Response) => {
+export interface LibraryFile
+{
+    path: string
+    url: string
+}
+
+let cache = apicache.middleware
+
+app.get("/download/:manufacturer/:model", cache('1 hour'), async (req: Request, res: Response) => {
 
     const octokit = new Octokit({
         auth: githubToken
     });
-    const manufacturer = req.query.manufacturer;
-    const model = req.query.model;
+    const manufacturer = req.params.manufacturer;
+    const model = req.params.model;
     if (!manufacturer || !model) {
         console.error("No manufacturer or model passed", manufacturer, model);
         res.status(422).json({message: "No manufacturer or model provided"});
         return;
     }
 
-    try {
+    const fetchContents = async (path: string, newPath: string|null = null): Promise<LibraryFile[]> => {
+        if (newPath === null) {
+            newPath = path
+        }
+
         const { data } = await octokit.repos.getContent({
             owner: owner,
             repo: repo,
-            path: "custom_components/powercalc/data/" + manufacturer + "/" + model,
+            path: newPath,
         });
+    
         if (!Array.isArray(data)) {
+            return []
+        }
+    
+        // If it's a directory, recursively fetch its contents
+        const subContents = await Promise.all(
+            data.map(async (item): Promise<LibraryFile[]> => {
+                if (item.type === "file") {
+                    // console.log(item)
+                    let regex = new RegExp(path, "g");
+                    let modifiedPath = item.path.replace(regex, '').substring(1);
+                    return [{ path: modifiedPath, url: item.download_url ?? '' }];
+                } else if (item.type === "dir") {
+                    const newPath = `${path}/${item.name}`;
+                    return await fetchContents(path, newPath);
+                } else {
+                    return []
+                }
+            })
+        );
+            
+        return subContents.flat()
+    };
+    
+    try {
+        const files = await fetchContents(
+            "custom_components/powercalc/data/" + manufacturer + "/" + model
+        );
+        if (files.length === 0) {
             console.error("No data found", manufacturer, model);
             res.status(404).json({message: "No download url's found"});
             return;
           }
           console.log("Data successfully retrieved", manufacturer, model);
-        res.json(data.map((x) => ({"filename": x.name, "url": x.download_url})))
+            res.json(files)
     } catch (error) {
         console.error("Model not found");
         res.status(404).json({message: "Model not found"});
     }
 });
 
+app.get("/library", cache('1 hour'), async (req: Request, res: Response) => {
+    const url = "https://raw.githubusercontent.com/bramstroker/homeassistant-powercalc/feat/library-download/custom_components/powercalc/data/library.json"
+    console.log("Fetching library")
+    const resp = await fetch(url)
+    res.json(await resp.json())
+});
+  
+  // add route to display cache index
+app.get('/cache/index', (req, res) => {
+    res.json(apicache.getIndex())
+})
+
+app.get('/cache/clear', (req, res) => {
+    res.json(apicache.clear([]))
+})
+
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
