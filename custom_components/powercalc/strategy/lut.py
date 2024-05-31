077b336233cd683a1dff1e96cf0493bb745c22ee
Jan-Philipp Benecke <jan-philipp@bnck.me>
2024-05-31 20:38:54 +0200

    Wrap in partial

--- custom_components/powercalc/strategy/lut.py
+++ custom_components/powercalc/strategy/lut.py
@@ -63,7 +63,7 @@ class LutRegistry:
 
             csv_file = await self._hass.async_add_executor_job(partial(self.get_lut_file, power_profile, color_mode))
             with csv_file:
-                csv_reader = reader(csv_file)  # type: ignore
+                csv_reader = reader(csv_file)
                 next(csv_reader)  # skip header row
 
                 line_count = 0
