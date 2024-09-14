266c5c33bc58b46a8e748a39ddd3b0df5714d441
Bram <bgerritsen@emico.nl>
2024-09-14 15:06:06 +0200

    fix: resolve sonarcube issues

--- utils/visualize/compare.py
+++ utils/visualize/compare.py
@@ -30,19 +30,6 @@ def csv_to_dataframe(csv_file: str) -> pd.DataFrame:
     return pd.read_csv(csv_file)
 
 
-# frames = [
-#     DataSet(csv_to_dataframe(get_library_file('lifx/LIFX Z/length_1/color_temp.csv.gz')), "blue", "1"),
-#     DataSet(csv_to_dataframe(get_library_file('lifx/LIFX Z/length_2/color_temp.csv.gz')), "red", "2"),
-#     DataSet(csv_to_dataframe(get_library_file('lifx/LIFX Z/length_3/color_temp.csv.gz')), "green", "3"),
-#     DataSet(csv_to_dataframe(get_library_file('lifx/LIFX Z/length_4/color_temp.csv.gz')), "yellow", "4"),
-#     DataSet(csv_to_dataframe(get_library_file('lifx/LIFX Z/length_5/color_temp.csv.gz')), "purple", "5"),
-#     # DataSet(csv_to_dataframe(get_library_file('lifx/LIFX Z/length_6/color_temp.csv.gz')), "orange", "6"),
-#     # DataSet(csv_to_dataframe(get_library_file('lifx/LIFX Z/length_7/color_temp.csv.gz')), "black", "7"),
-#     # DataSet(csv_to_dataframe(get_library_file('lifx/LIFX Z/length_8/color_temp.csv.gz')), "blue", "8"),
-#     # DataSet(csv_to_dataframe(get_library_file('lifx/LIFX Z/length_9/color_temp.csv.gz')), "red", "9"),
-#     # DataSet(csv_to_dataframe(get_library_file('lifx/LIFX Z/length_10/color_temp.csv.gz')), "green", "10"),
-# ]
-
 frames = [
     DataSet(csv_to_dataframe("LWA017.csv"), "blue", "1"),
     DataSet(csv_to_dataframe("brightness.csv"), "red", "2"),
