32d3da01ec7d06e89e303f6d6d6a8c0894d8640c
CV <dagobert@users.noreply.github.com>
2024-10-22 17:13:28 +0200

    Added asiases support for custom models
    Aliases are recognized for custom models. Association between model/alias and model path is generated on loader initialization.

--- custom_components/powercalc/power_profile/loader/local.py
+++ custom_components/powercalc/power_profile/loader/local.py
@@ -1,5 +1,6 @@
 import json
 import os
+import logging
 from typing import Any, cast
 
 from homeassistant.core import HomeAssistant
@@ -17,8 +18,13 @@ class LocalLoader(Loader):
         self._hass = hass
         self._manufacturer_listing: dict[str, set[str]] = {}
 
+        self._device_path_matcher = {}
+        self._LOGGER = logging.getLogger(__name__)
+
     async def initialize(self) -> None:
         """Initialize the loader."""
+        self._device_path_matcher = await self._load_custom_library()
+
 
     async def get_manufacturer_listing(self, device_type: DeviceType | None) -> set[str]:
         """Get listing of available manufacturers."""
@@ -76,22 +82,18 @@ class LocalLoader(Loader):
 
     async def load_model(self, manufacturer: str, model: str) -> tuple[dict, str] | None:
         """Load a model.json file from disk for a given manufacturer and model."""
-        base_dir = (
-            self._data_directory
-            if self._is_custom_directory
-            else os.path.join(
-                self._data_directory,
-                manufacturer.lower(),
-                model,
-            )
-        )
-
-        if not os.path.exists(base_dir):
+        
+        manufacturer_exists = self._device_path_matcher.get(manufacturer)
+        if not manufacturer_exists:
             return None
-
-        model_json_path = os.path.join(base_dir, "model.json")
-        if not model_json_path or not os.path.exists(model_json_path):
-            raise LibraryLoadingError(f"model.json not found for {manufacturer} {model}")
+        
+        model_dir = manufacturer_exists.get(model)
+        if not model_dir:
+            raise LibraryLoadingError(f"Model {model} not found")
+        
+        model_json_path = os.path.join(model_dir, "model.json")
+        if not os.path.exists(model_json_path):
+            raise LibraryLoadingError(f"model.json not found for {manufacturer} and {model} in {model_dir}")
 
         def _load_json() -> dict[str, Any]:
             """Load model.json file for a given model."""
@@ -99,15 +101,73 @@ class LocalLoader(Loader):
                 return cast(dict[str, Any], json.load(file))
 
         model_json = await self._hass.async_add_executor_job(_load_json)  # type: ignore
-        return model_json, base_dir
+        return model_json, model_dir
 
     async def find_model(self, manufacturer: str, search: set[str]) -> str | None:
         """Find a model for a given manufacturer. Also must check aliases."""
-        manufacturer_dir = os.path.join(self._data_directory, manufacturer)
-        if not os.path.exists(manufacturer_dir):
-            return None
 
-        model_dirs = await self._hass.async_add_executor_job(os.listdir, manufacturer_dir)
+        manufacturer_exists = self._device_path_matcher.get(manufacturer.lower())
+        if not manufacturer_exists:
+            self._LOGGER.info("Manufacturer does not exist in custom library: %s", manufacturer)
+            return None
+        
         search_lower = {phrase.lower() for phrase in search}
 
-        return next((model for model in model_dirs if model.lower() in search_lower), None)
+        return next((model for model in manufacturer_exists.keys() if model.lower() in search_lower), None)
+
+    async def _load_custom_library(self) -> dict:
+        """Loading custom models and aliases from file system"""
+
+        library = {}
+        base_dir = (
+            self._data_directory
+            if self._is_custom_directory
+            else os.path.join(
+                self._data_directory
+            )
+        )
+
+        if not os.path.exists(base_dir):
+            self._LOGGER.warning(f"Custom library directory does not exist: {base_dir}")
+            return library
+
+        manufacturers = await self._hass.async_add_executor_job(os.walk, base_dir)
+        manufacturers = await self._hass.async_add_executor_job(next, manufacturers)
+
+        def _load_json() -> dict[str, Any]:
+            """Load model.json file for a given model."""
+            with open(model_json_path) as file:
+                return cast(dict[str, Any], json.load(file))
+        
+        for manufacturer in manufacturers[1]:
+            manufacturer_dir = os.path.join(base_dir, manufacturer)
+            if not os.path.exists(manufacturer_dir):
+                self._LOGGER.error(f"Manufacturer directory {manufacturer_dir} should be there but is not!")
+                next
+
+            library[manufacturer] = {}
+        
+            models = await self._hass.async_add_executor_job(os.walk, manufacturer_dir)
+            models = await self._hass.async_add_executor_job(next, models)
+
+            for model in models[1]:
+                model_dir = os.path.join(manufacturer_dir, model)
+                if not os.path.exists(model_dir):
+                    self._LOGGER.error(f"Model directory {model_dir} should be there but is not!")
+                    next
+                
+                library[manufacturer][model] = model_dir
+
+                model_json_path = os.path.join(model_dir, "model.json")
+                if not os.path.exists(model_json_path):
+                    self._LOGGER.warning(f"model.json should exist in {model_dir}!")
+                    next
+
+                model_json = await self._hass.async_add_executor_job(_load_json)  # type: ignore
+
+                aliases = model_json.get("aliases")
+                if aliases:
+                    for alias in aliases:
+                        library[manufacturer][alias.lower()] = model_dir
+
+        return library
