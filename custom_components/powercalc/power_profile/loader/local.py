01dbc6f669c295d316e0d05544f0d53faaaa7a02
CV <dagobert@users.noreply.github.com>
2024-10-22 17:51:21 +0200

    Fixed trim trailing whitespace

--- custom_components/powercalc/power_profile/loader/local.py
+++ custom_components/powercalc/power_profile/loader/local.py
@@ -1,6 +1,6 @@
 import json
-import logging
 import os
+import logging
 from typing import Any, cast
 
 from homeassistant.core import HomeAssistant
@@ -11,7 +11,6 @@ from custom_components.powercalc.power_profile.power_profile import DeviceType
 
 _LOGGER = logging.getLogger(__name__)
 
-
 class LocalLoader(Loader):
     def __init__(self, hass: HomeAssistant, directory: str, is_custom_directory: bool = False) -> None:
         self._model_aliases: dict[str, dict[str, str]] = {}
@@ -20,7 +19,7 @@ class LocalLoader(Loader):
         self._hass = hass
         self._manufacturer_listing: dict[str, set[str]] = {}
 
-        self._device_path_matcher: dict[str, dict[str, str]] = {}
+        self._device_path_matcher: dict[str, dict[str, str]]= {}
 
     async def initialize(self) -> None:
         """Initialize the loader."""
@@ -123,7 +122,7 @@ class LocalLoader(Loader):
             self._data_directory
             if self._is_custom_directory
             else os.path.join(
-                self._data_directory,
+                self._data_directory
             )
         )
 
