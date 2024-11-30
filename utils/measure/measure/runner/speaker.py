7410313784c4198f9f7fb545152fb4df95d176ee
Bram <bgerritsen@emico.nl>
2024-11-30 09:41:36 +0100

    fix: setup config fixture correctly, cleanup and add workflow

--- utils/measure/measure/runner/speaker.py
+++ utils/measure/measure/runner/speaker.py
@@ -4,13 +4,12 @@ from typing import Any
 
 import inquirer
 
+from measure.config import MeasureConfig
 from measure.controller.media.controller import MediaController
 from measure.controller.media.factory import MediaControllerFactory
 from measure.powermeter.errors import ZeroReadingError
+from measure.runner.runner import MeasurementRunner, RunnerResult
 from measure.util.measure_util import MeasureUtil
-
-from ..config import MeasureConfig
-from .runner import MeasurementRunner, RunnerResult
 
 DURATION_PER_VOLUME_LEVEL = 20
 STREAM_URL = "https://powercalc.s3.eu-west-1.amazonaws.com/g_pink.mp3"
