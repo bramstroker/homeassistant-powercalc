554c743047f9d0a2fa79f613498ddf569e42b655
@RubenKelevra <cyrond@gmail.com>
2024-12-24 16:31:39 +0100

    Update utils/measure/measure/util/measure_util.py
    Co-authored-by: Bram Gerritsen <bgerritsen@gmail.com>

--- utils/measure/measure/util/measure_util.py
+++ utils/measure/measure/util/measure_util.py
@@ -149,10 +149,7 @@ class MeasureUtil:
             measurement: PowerMeasurementResult | None = None
 
             try:
-                if self.dummy_load_value:
-                    measurement = self.power_meter.get_power(include_voltage=True)
-                else:
-                    measurement = self.power_meter.get_power()
+                self.power_meter.get_power(include_voltage=bool(self.dummy_load_value))
 
                 updated_at = dt.fromtimestamp(measurement.updated).strftime(
                     "%d-%m-%Y, %H:%M:%S",
