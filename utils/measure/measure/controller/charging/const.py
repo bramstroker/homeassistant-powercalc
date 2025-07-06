42234e96c3a0fbaa4352d6c9ea09621235f7e837
Bram <bgerritsen@emico.nl>
2025-07-06 09:34:54 +0200

    feat: add choice for batter level attribute or entity

--- utils/measure/measure/controller/charging/const.py
+++ utils/measure/measure/controller/charging/const.py
@@ -10,4 +10,11 @@ class ChargingDeviceType(str, Enum):
     VACUUM_ROBOT = "vacuum_robot"
 
 
+class BatteryLevelSourceType(str, Enum):
+    ATTRIBUTE = "attribute"
+    ENTITY = "entity"
+
+
 QUESTION_BATTERY_LEVEL_ATTRIBUTE = "battery_level_attribute"
+QUESTION_BATTERY_LEVEL_ENTITY = "battery_level_entity"
+QUESTION_BATTERY_LEVEL_SOURCE_TYPE = "battery_level_source_type"
