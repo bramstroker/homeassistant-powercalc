f1082c88c74696acf529c4979f70dd75380cf819
Bram <bgerritsen@emico.nl>
2025-08-16 09:00:34 +0200

    fix: migration

--- custom_components/powercalc/sensor.py
+++ custom_components/powercalc/sensor.py
@@ -429,7 +429,7 @@ def save_entity_ids_on_config_entry(
     """
     _LOGGER.debug("Saving entity ids on config entry %s", sub_entry.subentry_id)
     power_entities = [e.entity_id for e in entities.all() if isinstance(e, VirtualPowerSensor)]
-    new_data = config_entry.data.copy()
+    new_data = sub_entry.data.copy()
     if power_entities:
         _LOGGER.debug("Setting power entity_id %s on config entry %s", power_entities[0], sub_entry.subentry_id)
         new_data.update({ENTRY_DATA_POWER_ENTITY: power_entities[0]})
