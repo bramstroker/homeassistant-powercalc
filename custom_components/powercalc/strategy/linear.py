24e7c47b3c072e6a68a91f0cf3b442b00d45a50b
Alexandre Bertrand <trandbert37@users.noreply.github.com>
2025-10-13 16:12:23 +0200

    Update custom_components/powercalc/strategy/linear.py
    Co-authored-by: Bram Gerritsen <bgerritsen@gmail.com>

--- custom_components/powercalc/strategy/linear.py
+++ custom_components/powercalc/strategy/linear.py
@@ -233,7 +233,7 @@ class LinearStrategy(PowerCalculationStrategyInterface):
     async def get_value_entity(self) -> SourceEntity:
         """Set the value entity based on the current state."""
         if (
-            (self._source_entity.domain == vacuum.DOMAIN or self._source_entity.domain == lawn_mower.DOMAIN)
+            self._source_entity.domain in (vacuum.DOMAIN, lawn_mower.DOMAIN)
             and self._attribute is None
             and self._source_entity.entity_entry
         ):
