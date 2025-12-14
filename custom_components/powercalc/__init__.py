bfd76501ae5b7af5f6e3c922e790de6ebdaedd7d
Bram <bgerritsen@emico.nl>
2025-12-14 11:36:37 +0100

    fix: change startup delay to match HA analytics collection

--- custom_components/powercalc/__init__.py
+++ custom_components/powercalc/__init__.py
@@ -232,7 +232,7 @@ async def init_analytics(hass: HomeAssistant) -> None:
         """Start the send schedule after the started event."""
         async_call_later(
             hass,
-            10,
+            900,
             HassJob(
                 analytics.send_analytics,
                 name="powercalc analytics startup",
