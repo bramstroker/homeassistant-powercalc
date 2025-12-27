33bdc3105780a24e0957d0ed61d8cef612f21b41
mathgaming <mathgaming@outlook.com>
2025-12-27 23:19:03 +0100

    Lint

--- utils/philips_hue_alias/add_alias.py
+++ utils/philips_hue_alias/add_alias.py
@@ -23,28 +23,28 @@ def parse_csv() -> dict:
 def add_alias(signify_devices: list[str], art_to_code: dict) -> None:
     for device in signify_devices:
         if device not in art_to_code:
-            print(f"device {device} not found in csv. Skipping it")
+            # device not found in csv. Skipping it"
             continue
 
         json_file = os.path.join(signify_path, device, "model.json")
 
-        with open(json_file, encoding="utf-8") as jsonFile:
-            data = json.load(jsonFile)
+        with open(json_file, encoding="utf-8") as json_file:
+            data = json.load(json_file)
 
         if "aliases" not in data:
             data["aliases"] = []
 
         data["aliases"] += art_to_code[device]
-        data["aliases"] = sorted(list(set(data["aliases"])))
+        data["aliases"] = sorted(set(data["aliases"]))
 
-        with open(json_file, "w", encoding="utf-8") as jsonFile:
-            json.dump(data, jsonFile, indent=2, sort_keys=True, ensure_ascii=False)
-            jsonFile.write("\n")
+        with open(json_file, "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file, indent=2, sort_keys=True, ensure_ascii=False)
+            json_file.write("\n")
 
 
 def main() -> None:
     if not os.path.exists(signify_path):
-        print("signify path not found. Did you run the script from the repo root dir?")
+        # signify path not found. Did you run the script from the repo root dir?
         sys.exit(0)
 
     signify_devices = [f.name for f in os.scandir(signify_path) if f.is_dir()]
