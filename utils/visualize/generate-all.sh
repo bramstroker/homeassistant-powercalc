9e4b073ea5d28465877c4bc0d7bfb903b64bd9e4
Bram <bgerritsen@emico.nl>
2026-01-10 11:29:47 +0100

    fix: bug with generate plots workflow

--- utils/visualize/generate-all.sh
+++ utils/visualize/generate-all.sh
@@ -33,16 +33,16 @@ process_file() {
   fi
 }
 
-# Find specific .csv.gz files recursively and loop over them
-for pattern in "color_temp.csv.gz" "brightness.csv.gz" "hs.csv.gz" "effects.csv.gz"; do
-  find "$SEARCH_DIR" -type f -name "$pattern" | while IFS= read -r file; do
-    echo "Processing $file"
-    relative_path=$(echo "$file" | sed "s|$SEARCH_DIR/||" | sed 's/.csv.gz$//')
-    output="$SEARCH_DIR/$relative_path.png"
-
-    process_file "$file" "$output"
-  done
-done
+## Find specific .csv.gz files recursively and loop over them
+#for pattern in "color_temp.csv.gz" "brightness.csv.gz" "hs.csv.gz" "effects.csv.gz"; do
+#  find "$SEARCH_DIR" -type f -name "$pattern" | while IFS= read -r file; do
+#    echo "Processing $file"
+#    relative_path=$(echo "$file" | sed "s|$SEARCH_DIR/||" | sed 's/.csv.gz$//')
+#    output="$SEARCH_DIR/$relative_path.png"
+#
+#    process_file "$file" "$output"
+#  done
+#done
 
 # Find all model.json files recursively and check if they have linear_config -> calibrate
 find "$SEARCH_DIR" -type f -name "model.json" | while IFS= read -r file; do
@@ -50,7 +50,7 @@ find "$SEARCH_DIR" -type f -name "model.json" | while IFS= read -r file; do
   dir_depth=$(echo "$relative_path" | tr -cd '/' | wc -c)
 
   # Skip sub profiles (3 or more directories deep)
-  if [ "$dir_depth" -eq 1 ] && grep -q "linear_config" "$file" && grep -q "calibrate" "$file"; then
+  if [ "$dir_depth" -eq 2 ] && grep -q "linear_config" "$file" && grep -q "calibrate" "$file"; then
     echo "Processing model.json file: $file"
     relative_path=$(echo "$relative_path" | sed 's/model.json$//')
     output="$SEARCH_DIR/${relative_path}calibration.png"
