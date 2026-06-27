50d7de05dc15ee2fd29ac90cc5088114b5605167
Bram Gerritsen <bgerritsen@gmail.com>
2026-06-27 20:12:38 +0200

    Extend precommit with more hooks (#4245)
    * chore: extend precommit with more hooks
    
    * chore: extend precommit with more hooks
    
    * chore: extend precommit with more hooks
    
    * chore: extend precommit with more hooks

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPTPATH="$(cd -- "$(dirname "$0")" >/dev/null 2>&1 || exit; pwd -P)"
 
-cd "$SCRIPTPATH/../custom_components"
+cd "$SCRIPTPATH/../custom_components" || exit
 ln -sf ../tests/testing_config/custom_components/test test
--- utils/visualize/generate-all.sh
+++ utils/visualize/generate-all.sh
@@ -41,13 +41,13 @@ done
 
 # Find all model.json files recursively and check if they have linear_config -> calibrate
 find "$SEARCH_DIR" -type f -name "model.json" | while IFS= read -r file; do
-  relative_path=$(echo "$file" | sed "s|$SEARCH_DIR/||")
+  relative_path="${file#"$SEARCH_DIR"/}"
   dir_depth=$(echo "$relative_path" | tr -cd '/' | wc -c)
 
   # Skip sub profiles (3 or more directories deep)
   if [[ "$dir_depth" -eq 2 ]] && grep -q "linear_config" "$file" && grep -q "calibrate" "$file"; then
     echo "Processing model.json file: $file"
-    relative_path=$(echo "$relative_path" | sed 's/model.json$//')
+    relative_path="${relative_path%model.json}"
     output="$SEARCH_DIR/${relative_path}calibration.png"
 
     process_file "$file" "$output"
