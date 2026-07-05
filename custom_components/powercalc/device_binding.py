86cfdf29ce67761fda20ae53ab1701a18eaf1ac7
Bram Gerritsen <bgerritsen@gmail.com>
2026-07-05 09:18:19 +0200

    Remove powercalc config entries from device (#4259)
    * fix: do not add powercalc config_entry to devices anymore
    
    * fix: do not add powercalc config_entry to devices anymore
    
    * style: apply pre-commit auto-fixes
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- custom_components/powercalc/device_binding.py
+++ custom_components/powercalc/device_binding.py
@@ -12,7 +12,7 @@ from homeassistant.helpers.entity_registry import RegistryEntry
 from homeassistant.helpers.typing import ConfigType
 
 from custom_components.powercalc.common import SourceEntity
-from custom_components.powercalc.const import CONF_AREA, CONF_SENSOR_TYPE, SensorType
+from custom_components.powercalc.const import CONF_AREA
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -28,57 +28,13 @@ async def attach_entities_to_resolved_device(
 
     device_entry = get_device_entry(hass, sensor_config, source_entity, config_entry)
     if not device_entry:
-        if config_entry:
-            sensor_type = SensorType(config_entry.data.get(CONF_SENSOR_TYPE, SensorType.VIRTUAL_POWER))
-            if sensor_type == SensorType.GROUP:
-                remove_stale_devices(hass, config_entry, None)
         return
-
-    if config_entry:
-        bind_config_entry_to_device(hass, config_entry, device_entry)
 
     for entity in entities_to_add:
         try:
             entity.device_entry = device_entry
         except AttributeError:  # pragma: no cover
             _LOGGER.error("%s: Cannot set device id on entity", entity.entity_id)
-
-
-def bind_config_entry_to_device(hass: HomeAssistant, config_entry: ConfigEntry, device_entry: DeviceEntry) -> None:
-    """
-    When the user selected a specific device in the config flow, bind the config entry to that device
-    This will let HA bind all the powercalc entities for that config entry to the concerning device
-    """
-
-    if config_entry.entry_id not in device_entry.config_entries:
-        device_reg = device_registry.async_get(hass)
-        device_reg.async_update_device(
-            device_entry.id,
-            add_config_entry_id=config_entry.entry_id,
-        )
-
-    remove_stale_devices(hass, config_entry, device_entry.id)
-
-
-def remove_stale_devices(
-    hass: HomeAssistant,
-    config_entry: ConfigEntry,
-    device_id: str | None,
-) -> None:
-    """Remove powercalc config entries from old devices."""
-    device_reg = device_registry.async_get(hass)
-    device_entries = device_registry.async_entries_for_config_entry(
-        device_reg,
-        config_entry.entry_id,
-    )
-
-    stale_devices = [device_entry for device_entry in device_entries if device_entry.id != device_id]
-
-    for device_entry in stale_devices:
-        device_reg.async_update_device(
-            device_entry.id,
-            remove_config_entry_id=config_entry.entry_id,
-        )
 
 
 def get_device_entry(
